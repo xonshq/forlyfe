04338e0b6ffab059dec85f8dc19766a76e984a50
Angelo Aldecoa <angelokn14@gmail.com>
2023-08-02 23:01:53 -0700

    set up

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import '../app.css';
+	import logo from '$lib/images/logo.png';
+	import logoMd from '$lib/images/logo-md.png';
+	import SlackIcon from '~icons/devicon/Slack';
+</script>
+
+<nav class="m-auto block max-w-screen-2xl">
+	<div class="flex items-center justify-between p-4">
+		<img src={logo} alt="test" class="w-20 md:hidden" />
+		<img src={logoMd} alt="test" class="hidden w-40 md:block" />
+		<div class="flex items-center space-x-6">
+			<a href="/events">Events</a>
+			<a href="https://slack.com" target="_blank" class="flex items-center"
+				><SlackIcon class="text-md mr-2" /><span>Community</span>
+			</a>
+		</div>
+	</div>
+</nav>
+
+<div class="m-auto h-20 max-w-screen-2xl p-4">
+	<slot />
+</div>
