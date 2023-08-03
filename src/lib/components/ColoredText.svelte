9d32269b0b59a68e814ac442daa17c290f74cf40
Angelo Aldecoa <angelokn14@gmail.com>
2023-08-03 01:35:28 -0700

    antialiased

--- src/lib/components/ColoredText.svelte
+++ src/lib/components/ColoredText.svelte
@@ -2,7 +2,6 @@
 	export let text: string;
 </script>
 
-<span
-	class="bg-gradient-to-r from-orange-400 via-orange-300 to-orange-600 bg-clip-text text-transparent"
+<span class="bg-gradient-to-r from-orange-400 to-orange-600 bg-clip-text text-transparent"
 	>{text}</span
 >
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1 class="text-4xl font-bold">Everyone has a story. Let's hear yours.</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import ColoredText from '$lib/components/ColoredText.svelte';
+</script>
+
+<h1 class="text-4xl font-bold">
+	<ColoredText text="Everyone has a story. Let's hear yours." />
+</h1>
