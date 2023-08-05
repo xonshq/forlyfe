77362c9438861a5b09015c15b47f0b748f87efc6
Angelo Aldecoa <angelokn14@gmail.com>
2023-08-05 00:31:42 -0700

    about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import BtnLink from '$lib/components/BtnLink.svelte';
+	import ColoredText from '$lib/components/ColoredText.svelte';
+</script>
+
+<div class="pt-20 mb-32 space-y-16 text-center md:pt-48">
+	<h1 class="text-5xl md:mb-20 md:text-7xl">
+		<ColoredText text="About Us" />
+	</h1>
+	<p class="text-2xl">
+		We are two college students attending NYU Stern, passionate about bridging the gap between tech
+		professionals and college students. We designed Fireside to provide a space for tech & VC
+		professionals to share their experiences, insights, and advice with the next generation of
+		innovators.
+	</p>
+	<BtnLink href="#" title="Join Our Community" />
+</div>
